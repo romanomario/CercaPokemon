2ce55b9719273ae3a07955c0cae3444fd5e8f010
Mario <marioromano1998@hotmail.com>
2021-09-28 01:11:02 +0200

    SeatchPokemon 0.2
    Done:
    - Italian language for types
    -Features adjusted
    - Provisional graphics
    To do:
    - Make responsive with mediaquery and using bootstra
    - Enter the title and the bar below
    -Search also with partial text (e.g. bulb = bulbasaur)
    -Send the result after 200ms (setTimeOut)
    -Insert the front and back of the Pokemon
    -Insert automatic focus on text input

--- script.js
+++ script.js
@@ -46,6 +46,7 @@ $(document).ready(function() {
     var userInput = $("#search").val();
     var url = "https://pokeapi.co/api/v2/pokemon/" + userInput;
     console.log(url);
+    document.getElementById('pokeOutput').style.display = "block";
     $.ajax({
       url: url,
       dataType: "json",
@@ -55,14 +56,39 @@ $(document).ready(function() {
           pokeImgFront = data.sprites.front_default,
           shiny = false,
           frontImg = true,
-          def = "<span class='stat'>Difesa: </span>" + data.stats[3].base_stat,
-          atk = "<span class='stat'>Attacco: </span>" + data.stats[4].base_stat,
-          hp = "<span class='stat'>PV: </span>" + data.stats[5].base_stat,
+          speed ="<span class='stat'>Velocità: </span>" + data.stats[0].base_stat ,
+          spDef ="<span class='stat'>Difesa speciale </span>" + data.stats[1].base_stat,
+          spAtk ="<span class='stat'>Attacco speciale: </span>" + data.stats[2].base_stat,
+          def ="<span class='stat'>Difesa: </span>" + data.stats[3].base_stat,
+          atk ="<span class='stat'>Attacco: </span>" + data.stats[4].base_stat,
+          hp ="<span class='stat'>PV: </span>" + data.stats[5].base_stat,
           id = "#" + data.id,
+          weight = "<span class='stat'>Peso: </span>" + data.weight,
+          height = "<span class='stat'>Altezza: </span>" + data.height,
           types = [];
 
         for (var i = 0; i < data.types.length; i++) {
+
           var type = data.types[i].type.name;
+          if(type == "normal") type = "normale";
+          else if(type == "grass") type = "erba";
+          else if(type == "ground") type = "terra";
+          else if(type == "rock") type = "roccia";
+          else if(type == "fighting") type = "lotta";
+          else if(type == "steel") type = "acciaio";
+          else if(type == "fire") type = "fuoco";
+          else if(type == "electric") type = "elettrico";
+          else if(type == "flying") type = "volante";
+          else if(type == "psychic") type = "psico";
+          else if(type == "bug") type = "insetto";
+          else if(type == "dragon") type = "drago";
+          else if(type == "water") type = "acqua";
+          else if(type == "ice") type = "ghiaccio";
+          else if(type == "poison") type = "veleno";
+          else if(type == "dark") type = "ombra";
+          else if(type == "ghost") type = "fantasma";
+          else if(type == "water") type = "folletto";
+        
           types.push(type);
         }
 
@@ -79,15 +105,12 @@ $(document).ready(function() {
           }
         }
 
-        $("#defaultBtn").click(function() {
-          $("#pokeImage").attr("src", pokeImgFront);
-          shiny = false;
-          frontImg = true;
-        });
-
         $(".name").html(name);
         $(".idNum").html(id);
         $("#pokeImage").attr("src", pokeImgFront);
+        $(".speed").html(speed);
+        $(".height").html(height);
+        $(".weight").html(weight);
         $(".hp").html(hp);
         $(".attack").html(atk);
         $(".defense").html(def);
