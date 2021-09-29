1b91c3e15ef2b936cf1ef8f73b7e0dc2933c2640
Mario <marioromano1998@hotmail.com>
2021-09-30 01:53:13 +0200

    Cerca Pokemon 0.5
    Done:
    -partial search work
    To do:
    - Make responsive with multimedia queries and using bootstrap
    - Make the partial search actually work
    -Insert a result of error if pokemon not found
    -Eliminate search bar suggestions
    - Deleting characters must also delete the search
    -Send the search also with the enter key

--- script.js
+++ script.js
@@ -26,114 +26,118 @@ $(document).ready(function() {
 
         //la stringa viene passata in "$(#search").val();"
         var userInput = $("#search").val();
+        Ricerca(userInput);
 
-        var url = "https://pokeapi.co/api/v2/pokemon/" + userInput;
+        function Ricerca(userInput){
+            var url = "https://pokeapi.co/api/v2/pokemon/" + userInput;
+            document.getElementById('pokeOutput').style.display = "block";
+            $.ajax({
+                url: url,
+                dataType: "json",
+                method: "GET",
+                success: function(data) {
+                    var name = data.forms[0].name,
+                        pokeImgFront = data.sprites.front_default,
+                        pokeImgBack = data.sprites.back_default,
+                        frontImg = true,
+                        speed = "<span class='stat'>Velocità: </span>" + data.stats[0].base_stat,
+                        spDef = "<span class='stat'>Difesa speciale </span>" + data.stats[1].base_stat,
+                        spAtk = "<span class='stat'>Attacco speciale: </span>" + data.stats[2].base_stat,
+                        def = "<span class='stat'>Difesa: </span>" + data.stats[3].base_stat,
+                        atk = "<span class='stat'>Attacco: </span>" + data.stats[4].base_stat,
+                        hp = "<span class='stat'>PV: </span>" + data.stats[5].base_stat,
+                        id = "#" + data.id,
+                        weight = "<span class='stat'>Peso: </span>" + data.weight,
+                        height = "<span class='stat'>Altezza: </span>" + data.height,
+                        types = [];
 
+                    for (var i = 0; i < data.types.length; i++) {
 
-        document.getElementById('pokeOutput').style.display = "block";
-        $.ajax({
-            url: url,
-            dataType: "json",
-            method: "GET",
-            success: function(data) {
-                var name = data.forms[0].name,
-                    pokeImgFront = data.sprites.front_default,
-                    pokeImgBack = data.sprites.back_default,
-                    frontImg = true,
-                    speed = "<span class='stat'>Velocità: </span>" + data.stats[0].base_stat,
-                    spDef = "<span class='stat'>Difesa speciale </span>" + data.stats[1].base_stat,
-                    spAtk = "<span class='stat'>Attacco speciale: </span>" + data.stats[2].base_stat,
-                    def = "<span class='stat'>Difesa: </span>" + data.stats[3].base_stat,
-                    atk = "<span class='stat'>Attacco: </span>" + data.stats[4].base_stat,
-                    hp = "<span class='stat'>PV: </span>" + data.stats[5].base_stat,
-                    id = "#" + data.id,
-                    weight = "<span class='stat'>Peso: </span>" + data.weight,
-                    height = "<span class='stat'>Altezza: </span>" + data.height,
-                    types = [];
+                        var type = data.types[i].type.name;
+                        if (type == "normal") type = "normale";
+                        else if (type == "grass") type = "erba";
+                        else if (type == "ground") type = "terra";
+                        else if (type == "rock") type = "roccia";
+                        else if (type == "fighting") type = "lotta";
+                        else if (type == "steel") type = "acciaio";
+                        else if (type == "fire") type = "fuoco";
+                        else if (type == "electric") type = "elettrico";
+                        else if (type == "flying") type = "volante";
+                        else if (type == "psychic") type = "psico";
+                        else if (type == "bug") type = "insetto";
+                        else if (type == "dragon") type = "drago";
+                        else if (type == "water") type = "acqua";
+                        else if (type == "ice") type = "ghiaccio";
+                        else if (type == "poison") type = "veleno";
+                        else if (type == "dark") type = "ombra";
+                        else if (type == "ghost") type = "fantasma";
+                        else if (type == "water") type = "folletto";
 
-                for (var i = 0; i < data.types.length; i++) {
-
-                    var type = data.types[i].type.name;
-                    if (type == "normal") type = "normale";
-                    else if (type == "grass") type = "erba";
-                    else if (type == "ground") type = "terra";
-                    else if (type == "rock") type = "roccia";
-                    else if (type == "fighting") type = "lotta";
-                    else if (type == "steel") type = "acciaio";
-                    else if (type == "fire") type = "fuoco";
-                    else if (type == "electric") type = "elettrico";
-                    else if (type == "flying") type = "volante";
-                    else if (type == "psychic") type = "psico";
-                    else if (type == "bug") type = "insetto";
-                    else if (type == "dragon") type = "drago";
-                    else if (type == "water") type = "acqua";
-                    else if (type == "ice") type = "ghiaccio";
-                    else if (type == "poison") type = "veleno";
-                    else if (type == "dark") type = "ombra";
-                    else if (type == "ghost") type = "fantasma";
-                    else if (type == "water") type = "folletto";
-
-                    types.push(type);
-                }
-
-                function pokemonType(types) {
-                    $("#types").html("");
-                    for (var i = 0; i < types.length; i++) {
-                        $("#types").append(
-                            "<div class='pokeType poke-info " +
-                            types[i] +
-                            "'>" +
-                            types[i] +
-                            " </div>"
-                        );
+                        types.push(type);
                     }
-                }
 
-                $(".changeBtn").click(function() {
-                    if (frontImg == true) {
-                        frontImg = false;
-                        $("#pokeImage").attr("src", pokeImgBack);
-                    } else if (frontImg == false) {
-                        frontImg = true;
-                        $("#pokeImage").attr("src", pokeImgFront);
-                    }
-                });
-
-                $(".name").html(name);
-                $(".idNum").html(id);
-                $("#pokeImage").attr("src", pokeImgFront);
-                $(".speed").html(speed);
-                $(".height").html(height);
-                $(".weight").html(weight);
-                $(".hp").html(hp);
-                $(".attack").html(atk);
-                $(".defense").html(def);
-                pokemonType(types);
-            }, //SUCCESS
-            error: function(xhr, ajaxOptions, thrownError) {
-                if (xhr.status == 404) {
-                    console.log("siamo nel 404 mfk");
-                    var url2 = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=900";
-                    $.ajax({
-                        url: url2,
-                        dataType: "json",
-                        method: "GET",
-                        success: function(data2) {
-                            var len = $("#search").val().length;
-                            for (var i = 0, res = false; i < data2.results.length && res == false; i++) {
-                                var x = data2.results[i].name.substr(0, len);
-                                if ($("#search").val() == x) {
-                                    console.log("Forse intendevi " + data2.results[i].name);
-                                    res = true;
-                                }
-                            }
+                    function pokemonType(types) {
+                        $("#types").html("");
+                        for (var i = 0; i < types.length; i++) {
+                            $("#types").append(
+                                "<div class='pokeType poke-info " +
+                                types[i] +
+                                "'>" +
+                                types[i] +
+                                " </div>"
+                            );
                         }
-                    })
-                }
-            }
-        }); //AJAX
-    }, 500));
+                    }
 
+                    $(".changeBtn").click(function() {
+                        if (frontImg == true) {
+                            frontImg = false;
+                            $("#pokeImage").attr("src", pokeImgBack);
+                        } else if (frontImg == false) {
+                            frontImg = true;
+                            $("#pokeImage").attr("src", pokeImgFront);
+                        }
+                    });
+
+                    $(".name").html(name);
+                    $(".idNum").html(id);
+                    $("#pokeImage").attr("src", pokeImgFront);
+                    $(".speed").html(speed);
+                    $(".height").html(height);
+                    $(".weight").html(weight);
+                    $(".hp").html(hp);
+                    $(".attack").html(atk);
+                    $(".defense").html(def);
+                    pokemonType(types);
+                }, //SUCCESS
+                error: function(xhr, ajaxOptions, thrownError) {
+                    if (xhr.status == 404) {
+                        console.log("siamo nel 404 mfk");
+                        RicercaParziale(userInput);
+                    }
+                }
+            }); } //AJAX 
+            function RicercaParziale(userInput){
+                var url2 = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=900";
+                        $.ajax({
+                            url: url2,
+                            dataType: "json",
+                            method: "GET",
+                            success: function(data2) {
+                                var len = userInput.length;
+                                for (var i = 0, res = false; i < data2.results.length && res == false; i++) {
+                                    var x = data2.results[i].name.substr(0, len);
+                                    if ($("#search").val() == x) {
+                                        console.log("Forse intendevi " + data2.results[i].name);
+                                        res = true;
+                                    }
+                                }
+                                 Ricerca(data2.results[i-1].name);            
+                            }
+                         
+                        })
+            }
+        }, 200));
     function delay(callback, ms) {
         var timer = 0;
         return function() {
