a9bc4dac8d92f54ba45979c0bde21762fd491a7f
Mario <marioromano1998@hotmail.com>
2021-10-12 15:55:37 +0200

    Cerca Pokemon 0.9
    done:
    -do not rely on "form", but on input text
    -use fetch not $ .ajax
    To do:
    -shows the API loading
    -do not use copied delay function, but use clearTimeout and setTimeout well
    -shows all pokemon achievements
    -perform type translations using associative array before even making any requests
    -do not use "var" but use "const" and "let"
    -Make responsive with multimedia queries and using bootstrap
    -Send the search also with the enter key
    -problem with the type of electric Pokémon

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
 $(document).ready(function() {
-    $("form").keyup(delay(function(e) {
+    $("input").keyup(delay(function(e) {
         e.preventDefault();
-
-        function getTitle(text) {
-            return text.match("<title>(.*)?</title>")[1];
-        }
-        // Realizzare la seguente richiesta CORS
 
         //la stringa viene passata in "$(#search").val();"
         var userInput = $("#search").val();
@@ -14,33 +9,28 @@ $(document).ready(function() {
         if(!userInput){
             document.getElementById('pokeOutput').style.display = "none";
 
-        } else Ricerca(userInput);
+        } else RicercaFetch(userInput);
 
-        function Ricerca(userInput){
-            var url = "https://pokeapi.co/api/v2/pokemon/" + userInput;
+        /** nuovo tipo di ricerca usando il fetch e non più il json */
+        function RicercaFetch(userInput){
+            var urlf = "https://pokeapi.co/api/v2/pokemon/" + userInput; 
+            fetch(urlf).then(response => {
+                if (response.ok) {
+                    response.json().then(data => {
 
-            $.ajax({
-                url: url,
-                dataType: "json",
-                method: "GET",
-                success: function(data) {
-                    document.getElementById('pokeOutput').style.display = "block";
+                        //metodo corretto per prendere i dati in json da questo metdono di merda
+                        document.getElementById('pokeOutput').style.display = "block";
                     document.getElementById('errorOut').style.display = "none";
 
-
-                    var name = data.forms[0].name,
+                        var name = data.forms[0].name,
                         pokeImgFront = data.sprites.front_default,
                         pokeImgBack = data.sprites.back_default,
                         frontImg = true,
                         speed = "<span class='stat'>Velocità: </span>" + data.stats[0].base_stat,
-                        spDef = "<span class='stat'>Difesa speciale </span>" + data.stats[1].base_stat,
-                        spAtk = "<span class='stat'>Attacco speciale: </span>" + data.stats[2].base_stat,
                         def = "<span class='stat'>Difesa: </span>" + data.stats[3].base_stat,
                         atk = "<span class='stat'>Attacco: </span>" + data.stats[4].base_stat,
                         hp = "<span class='stat'>PV: </span>" + data.stats[5].base_stat,
                         id = "#" + data.id,
-                        weight = "<span class='stat'>Peso: </span>" + data.weight,
-                        height = "<span class='stat'>Altezza: </span>" + data.height,
                         types = [];
 
                     for (var i = 0; i < data.types.length; i++) {
@@ -95,27 +85,36 @@ $(document).ready(function() {
                     $(".idNum").html(id);
                     $("#pokeImage").attr("src", pokeImgFront);
                     $(".speed").html(speed);
-                    $(".height").html(height);
-                    $(".weight").html(weight);
                     $(".hp").html(hp);
                     $(".attack").html(atk);
                     $(".defense").html(def);
                     pokemonType(types);
-                }, //SUCCESS
-                error: function(xhr, ajaxOptions, thrownError) {
-                    if (xhr.status == 404) {
-                        RicercaParziale(userInput);
-                    }
+                    });
+                //successo finito
                 }
-            }); } //AJAX 
-            function RicercaParziale(userInput){
-                var url2 = "https://pokeapi.co/api/v2/pokemon?offset=0&limit=900";
-                        $.ajax({
-                            url: url2,
-                            dataType: "json",
-                            method: "GET",
-                            success: function(data2) {
-                                var len = userInput.length;
+                if (response.status >= 100 && response.status < 200) {
+                   console.log("Informazioni per il client");
+                }
+                if (response.status >= 300 && response.status < 399) {
+                   console.log("Redirezione");
+                }
+                if (response.status >= 400 && response.status < 499) {
+                    //mettere qui l'errore e la chiamata a ricerca parziale
+                   console.log("Richiesta errata");
+                   RicercaParzialeFetch(userInput);
+                }
+                if (response.status >= 500 && response.status < 599) {
+                   console.log("Errore sul server");
+                }
+            }).catch(error => console.log("Si è verificato un errore!"))
+        }   
+            //la ricarca parziale deve essere sempre evocata e illustrare più tipi di pokemon 
+            function RicercaParzialeFetch(userInput){
+                fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=900").then(response => {
+                if (response.ok) {
+                    console.log("Contenuto ricevuto nella ricerca parziale");
+                    response.json().then(data2 => {
+                        var len = userInput.length;
                                 var res = false;
                                 for (var i = 0; i < data2.results.length && res == false; i++) {
                                     var x = data2.results[i].name.substr(0, len);
@@ -127,16 +126,31 @@ $(document).ready(function() {
                                 }
 
                             if(res){
-                                Ricerca(data2.results[i-1].name);
-                            }else
+                                RicercaFetch(data2.results[i-1].name);
+                            }else{
                                 document.getElementById('pokeOutput').style.display = "none";
                                 var error = "<span class='stat'>Spiacenti non è stato trovato nessun pokemon: </span>" + userInput;
                                 $(".error").html(error);
                                 document.getElementById('errorOut').style.display = "block";
-                            } 
-                        })
+                            }
+                      });
+                }
+                if (response.status >= 100 && response.status < 200) {
+                console.log("Informazioni per il client nella ricerca parziale");
+                }
+                if (response.status >= 300 && response.status < 399) {
+                console.log("Redirezione nella ricerca parziale");
+                }
+                if (response.status >= 400 && response.status < 499) {
+                console.log("Richiesta errata nella ricerca parziale");
+                }
+                if (response.status >= 500 && response.status < 599) {
+                console.log("Errore sul server nella ricerca parziale");
+                }
+            }).catch(error => console.log("Si è verificato un errore nella ricerca parziale!"))
             }
-        }, 200));
+        }, 300));
+
     function delay(callback, ms) {
         var timer = 0;
         return function() {
@@ -148,5 +162,4 @@ $(document).ready(function() {
             }, ms || 0);
         };
     }
-    //FORM
 });
